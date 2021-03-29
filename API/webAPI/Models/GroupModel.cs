63cd64705fc4482c7b6bc8d5b1624e591eb48a73
vmagen <vmagen@gmail.com>
2021-03-29 16:17:38 +0300

    add group

---
+++ API/webAPI/Models/GroupModel.cs
@@ -0,0 +1,16 @@
+﻿using DATA.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webAPI.Models
+{
+    public class GroupModel
+    {
+        public static List<RV_Group> GetAllGroups(ArvinoDbContext db)
+        {
+            return db.RV_Group.ToList();
+        }
+    }
+}
