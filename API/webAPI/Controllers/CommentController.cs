832a1d06d250098ec92aa46cc5091825b0a48147
RonNavon <ronyhezkel@gmail.com>
2021-04-02 17:36:58 +0300

    ron

---
+++ API/webAPI/Controllers/CommentController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using DATA.EF;
+using webAPI.Models;
+using webAPI.DTO;
+
+namespace webAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class CommentController : ApiController
+    {
+        public static ArvinoDbContext db = new ArvinoDbContext();
+
+        /// <summary>
+        /// https://localhost:44370/api/Comment
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public IHttpActionResult Post([FromBody] RV_Comment value)
+        {
+            try
+            {
+                RV_Comment comment = new RV_Comment()
+                {
+                    commentDate = DateTime.Now,
+                    commentTime = DateTime.Now,
+                    content = value.content,
+                    wineId = value.wineId,
+                    respondedUserEmail = value.respondedUserEmail,
+
+                };
+                db.RV_Comment.Add(comment);
+                db.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+    }
+}
